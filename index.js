c3155f8dc0eba37009ddc9259bb88e0262beb354
Adarsh Kumar Singh <02adarshrajput@gmail.com>
2025-07-30 18:31:54 +0530

    Fix: use MONGODB_URI for Mongo connection

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log("MongoDB connected"))
